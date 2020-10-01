7d2c34aa280e9bb5f55a3d69f68ba5f8ba020cb7
Shubhanshu Jain <51177359+shubhanshuj22@users.noreply.github.com>
2020-10-02 04:14:51 +0530

    Some updates
    Firstly I converted whole program into functional code for better understanding. The bug in Hangman is that if we enter a full word it's getting accepted so what if the user just input whole 26 letters("abcdefghijklmnopqrstuvwxyz") the game over and the player wins and another bug is that it counts miss for the same character input this means if user input 's' which is not there but if again user input 's' it's counting as a wrong input and user will lose one more chance to guess. Bugs in Stone Paper Scissor is that what if the user input "PaPeR" or "YeS" so I had converted the input into a small letter string using lower(). Dice Roll having the same bug as Stone paper game for "YeS". Guess the number having the same bugs as Hangman for string "0123456789" and repeating the wrong digit.

--- question.py
+++ question.py
@@ -1,21 +1,22 @@
 import random
 import string
-print("We are going to play games\n")
-print("Enter 1 to play HANGMAN")
-print("Enter 2 to play STONE-PAPER-SCISSOR")
-print("Enter 3 to play DICE ROLL")
-print("Enter 4 to play GUESS THE NUMBER")
-choice=int(input("Enter the number you want to play\n"))
-if choice==1:
+
+def Hangman():
     print("WELCOME TO HANGMAN GAME")
     print("\nYOU HAVE TO GUESS THE CHARACTERS OF HIDDEN WORD\n")
     letter=["hello","hangman","word","story","books","cooking"]
     word=random.choice(letter)
     turns=10
     words=""
+    ls = []
+    print(word)
     while turns>0:
         count=0
-        game=input("Character is :")
+        game=input("Character is :")[0]
+        if game in ls:
+            print("Already entered Character")
+            continue
+        ls.append(game)
         words=words+game
         if game not in word:
             turns=turns-1
@@ -27,23 +28,26 @@ if choice==1:
             break
         for i in word:
             if i in words:
-                print(i)
+                print(i,end = " ")
             else:
-                print("*")
+                print("*",end = " ")
                 count=count+1
+        print()
         if count==0:
             print("You won")
             break
     print("Word is :",word)
     print("Thanks for playing the game")
-if choice==2:
+
+
+def StonePaperScissor():
     print("WELCOME TO GAME")
     print("Player VS Computer")
     print("STONE PAPER SCISSOR")
     choice="yes"
     while choice=="yes" or choice=="y":
         print("\nEnter your choice")
-        n=input()
+        n=input().lower()
         x="scissor","stone","paper"
         print("\nComputer choice is")
         comp=random.choice(x)
@@ -58,14 +62,16 @@ if choice==2:
             print("Draw")
         else:
             print("Computer won you lose")
-        choice=input("\nDo u want to continue") 
+        choice=input("\nDo u want to continue").lower()
     print("Thanks for playing the game")
-if choice==3:
+
+
+def DiceRoll():
     print("WELCOME TO ROLLONG THE DICE")
     choice="yes"
     l=[]
     n=[]
-    while choice=="Yes" or choice=="yes":
+    while choice=="yes" or choice=="y":
         print("ROLLING THE DICE")
         print("You are player 1 ")
         t=random.randint(1,6)
@@ -80,7 +86,8 @@ if choice==3:
             print("You are leading this time")
         else:
             print("Equal")
-        choice=input("Do you want to continue")
+        choice=input("Do you want to continue 'Yes'")
+        choice = choice.lower()
     sum1=0
     sum2=0
     for i in l:
@@ -93,14 +100,19 @@ if choice==3:
         print("You loose and your score is :",sum1,"computer score is :",sum2)
     print("Thank you for playing the game")
 
-if choice==4:
+def GuessTheNumber():
     print("WELCOME TO GUESS THE NUMBER")
     num=str(random.randint(10000,1000000))
     turns=5
     numbers=""
+    ls = []
     while turns>0:
         count=0
-        charc=input("Enter the number:")
+        charc=input("Enter the digit:")[0]
+        if charc in ls:
+               print("Already entered this digit")
+               continue;
+        ls.append(charc)
         numbers=numbers+charc
         if charc not in num:
             turns=turns-1
@@ -112,13 +124,31 @@ if choice==4:
             break
         for i in num:
             if i in numbers:
-                print(i)
+                print(i,end = "")
             else:
-                print("*")
+                print("*",end = "")
                 count=count+1
+        print()  
         if count==0:
             print("You won")
             break
     print("Number is :",num)
     print("Thank you for playing the game")
+
+def main():    
+    print("We are going to play games\n")
+    print("Enter 1 to play HANGMAN")
+    print("Enter 2 to play STONE-PAPER-SCISSOR")
+    print("Enter 3 to play DICE ROLL")
+    print("Enter 4 to play GUESS THE NUMBER")
+    choice=int(input("Enter the number you want to play\n"))
+    if choice==1:
+        Hangman();
+    if choice==2:
+        StonePaperScissor()
+    if choice==3:
+        DiceRoll()
+    if choice==4:
+        GuessTheNumber()
+main()
 
